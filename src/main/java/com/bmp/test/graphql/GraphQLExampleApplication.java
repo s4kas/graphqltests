612eb70dc09a8e91c98a0d19b6014745645cd729
Bruno Marques <brunofgmarques@gmail.com>
2019-10-28 22:28:01 +0000

    tests

---
+++ src/main/java/com/bmp/test/graphql/GraphQLExampleApplication.java
@@ -0,0 +1,13 @@
+package com.bmp.test.graphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphQLExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphQLExampleApplication.class, args);
+    }
+
+}
